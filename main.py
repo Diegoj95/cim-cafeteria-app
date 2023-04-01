1ca33e81ee6c67bb5e6c60484673407b9f1780f1
Diegoj95 <dijm0001@red.ujaen.es>
2023-04-01 02:47:23 -0300

    call to Google sheets and interface

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 
+creds = ServiceAccountCredentials.from_json_keyfile_name('gs_credentials.json', scope)
 
-class cimApp(App):
+client = gspread.authorize(creds)
+
+sheet = client.open('DatosCafeteria').sheet1
+
+Builder.load_file('interfaz.kv')
+
+class Interfaz(BoxLayout):
+
+    def on_button_press(self):
+        # Retrieve the last row of data from the sheet
+        data = sheet.get_all_values()[-1]
+
+        # Update the labels with the new data
+        self.ids.mesas_disponibles.text = f'Mesas disponibles: {data[0]}'
+        self.ids.mesas_ocupadas.text = f'Mesas ocupadas: {data[1]}'
+        self.ids.fecha.text = f'Fecha/hora: {data[2]}'
+
+class MyApp(App):
     def build(self):
-        return Button(text="Hola Mundo!")
+        return Interfaz()
 
-
-if __name__ == "__main__":
-    cimApp().run()
+if __name__ == '__main__':
+    MyApp().run()
